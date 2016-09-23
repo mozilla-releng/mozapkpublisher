76fe741a28a409e0e15ffeedc43751c2cadb2a68
Johan Lorenzo <jlorenzo@mozilla.com>
2016-09-23 15:50:05 +0200

    Bug 1305045 - Part 3: Add package definition

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup, find_packages
+
+project_dir = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(project_dir, 'version.txt')) as f:
+    version = f.read().rstrip()
+
+# We're using a pip8 style requirements file, which allows us to embed hashes
+# of the packages in it. However, setuptools doesn't support parsing this type
+# of file, so we need to strip those out before passing the requirements along
+# to it.
+with open(os.path.join(project_dir, 'requirements.txt')) as f:
+    requirements = [line.split()[0] for line in f if not line.startswith('#')]
+
+setup(
+    name='mozapkpublisher',
+    version=version,
+    description='Scripts to get and push Firefox for Android to Google Play Store',
+    author='Mozilla Release Engineering',
+    author_email='release+python@mozilla.com',
+    url='https://github.com/mozilla-releng/mozapkpublisher',
+    packages=find_packages(),
+    license='MPL2',
+    install_requires=requirements,
+)
