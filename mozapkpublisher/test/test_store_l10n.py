98bff5d2f55568e2bcb4a397e57d7296777e6b9f
Francesco Lodolo (:flod) <flod@lodolo.net>
2017-01-03 11:51:25 +0100

    Update tests

--- mozapkpublisher/test/test_store_l10n.py
+++ mozapkpublisher/test/test_store_l10n.py
@@ -19,7 +19,7 @@ def test_get_list_locales(monkeypatch):
     monkeypatch.setattr(
         utils, 'load_json_url',
         lambda url: [u'en-GB', u'es-ES']
-        if url == 'https://l10n.mozilla-community.org/stores_l10n/api/google/listing/beta/' else None
+        if url == 'https://l10n.mozilla-community.org/stores_l10n/api/v1/fx_android/listing/beta/' else None
     )
     assert get_list_locales('beta') == [u'en-GB', u'es-ES']
 
@@ -32,7 +32,7 @@ def test_get_translation(monkeypatch):
             'long_desc': 'Long description',
             'short_desc': 'Short',
             'whatsnew': 'Check out this cool feature!'
-        } if url == 'https://l10n.mozilla-community.org/stores_l10n/api/google/translation/beta/en-GB/' else None
+        } if url == 'https://l10n.mozilla-community.org/stores_l10n/api/v1/fx_android/translation/beta/en-GB/' else None
     )
     assert get_translation('beta', 'en-GB') == {
         'title': 'Firefox for Android Beta',
@@ -47,7 +47,7 @@ def test_locale_mapping(monkeypatch):
     mock_json_url.side_effect = lambda url: {
         u'en-GB': u'en-GB',
         u'es-MX': u'es-US',
-    } if url == 'https://l10n.mozilla-community.org/stores_l10n/api/google/localesmapping/?reverse' else None
+    } if url == 'https://l10n.mozilla-community.org/stores_l10n/api/v1/google/localesmapping/?reverse' else None
 
     monkeypatch.setattr(utils, 'load_json_url', mock_json_url)
     assert locale_mapping('en-GB') == 'en-GB'
