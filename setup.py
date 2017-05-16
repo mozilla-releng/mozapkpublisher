e58ab1bd0d25eccb7977e27d272bfcc4b585c0ab
Aki Sasaki <aki@mozilla.com>
2017-05-16 09:53:11 -0700

    Unpin requirements from setup.py, since we pin in puppet (#28)
    Also added cache_discovery=False to silence spurious oauth2client errors

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(project_dir, 'version.txt')) as f:
 # of the packages in it. However, setuptools doesn't support parsing this type
 # of file, so we need to strip those out before passing the requirements along
 # to it.
-with open(os.path.join(project_dir, 'requirements.txt')) as f:
+with open(os.path.join(project_dir, 'requirements-dev.txt')) as f:
     requirements = [line.split()[0] for line in f if not line.startswith('#')]
 
 setup(
