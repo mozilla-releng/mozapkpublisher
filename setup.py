deb0fe987a42e298229dfe477ed9bc6f9b76c9ac
Johan Lorenzo <jlorenzo@mozilla.com>
2018-07-09 08:40:59 +0200

    Always use known requirements (#76)
    * Define several requirements.txt files with full dependencies (on per each build/test step). This is now the default way to build mozapkpublisher.
    * Remove dependencies that are only used by scriptworker and pushapkscript
    * Add `requirements.txt.in` to track needed subsets of deps. This can be use to regenerate the full list of needed dependencies.
    * Configure .coveragerc to exclude tests.
    * Make coveralls not silently fail anymore

--- setup.py
+++ setup.py
@@ -6,12 +6,16 @@ project_dir = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(project_dir, 'version.txt')) as f:
     version = f.read().rstrip()
 
-# We're using a pip8 style requirements file, which allows us to embed hashes
+# We're using a pip style requirements file, which allows us to embed hashes
 # of the packages in it. However, setuptools doesn't support parsing this type
 # of file, so we need to strip those out before passing the requirements along
 # to it.
-with open(os.path.join(project_dir, 'requirements-dev.txt')) as f:
-    requirements = [line.split()[0] for line in f if not line.startswith('#')]
+with open(os.path.join(project_dir, 'requirements.txt')) as f:
+    requirements = [
+        line.split()[0]
+        for line in f
+        if not (line.startswith('#') or '--hash=' in line)
+    ]
 
 setup(
     name='mozapkpublisher',
