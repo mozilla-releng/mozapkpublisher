6664c5a1f95895dfd23fc695d13709513ad8e2c0
Johan Lorenzo <jlorenzo@mozilla.com>
2018-11-07 19:01:01 +0100

    Use a simpler requirements files in setup.py
    This avoids bad formatting

--- setup.py
+++ setup.py
@@ -6,15 +6,11 @@ project_dir = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(project_dir, 'version.txt')) as f:
     version = f.read().rstrip()
 
-# We're using a pip style requirements file, which allows us to embed hashes
-# of the packages in it. However, setuptools doesn't support parsing this type
-# of file, so we need to strip those out before passing the requirements along
-# to it.
-with open(os.path.join(project_dir, 'requirements.txt')) as f:
+with open(os.path.join(project_dir, 'requirements.txt.in')) as f:
     requirements = [
         line.split()[0]
         for line in f
-        if not (line.startswith('#') or '--hash=' in line)
+        if not line.startswith('#')
     ]
 
 setup(
