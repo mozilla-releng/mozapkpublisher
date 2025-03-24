mozilla-releng/mozapkpublisher

commit 1d4775dffde4a82ee3dca257f1f07d42c5a6f8c9

Removes the "base" config parsing abstraction

mozapkpublisher/get_apk.py
@@ -9,11 +9,11 @@ import signal
 import shutil
 import logging
 
+from argparse import ArgumentParser
 from mozapkpublisher.common.apk.history import (
     get_expected_api_levels,
     get_firefox_major_version_number,
 )
-from mozapkpublisher.common.base import Base, ArgumentParser
 from mozapkpublisher.common.exceptions import CheckSumMismatch
 from mozapkpublisher.common.utils import (
     download_file,
@@ -25,34 +25,19 @@ from mozapkpublisher.common.utils import (
 logger = logging.getLogger(__name__)
 
 FTP_BASE_URL = 'https://ftp.mozilla.org/pub/mobile'
+ARCH_VALUES = ["arm", "x86"]
+JSON_VERSION_URL = "https://product-details.mozilla.org/1.0/firefox_versions.json"
 
 
-class GetAPK(Base):
-    arch_values = ["arm", "x86"]
-
-    json_version_url = "https://product-details.mozilla.org/1.0/firefox_versions.json"
-
-    @classmethod
-    def _init_parser(cls):
-        cls.parser = ArgumentParser(
-            description='Download APKs of Firefox for Android (aka Fennec) from {}'.format(FTP_BASE_URL)
-        )
-
-        exclusive_group = cls.parser.add_mutually_exclusive_group(required=True)
-        exclusive_group.add_argument('--version', default=None, help='Specify version number to download (e.g. 23.0b7)')
-        exclusive_group.add_argument('--latest-nightly', action='store_true', default=False,
-                                     help='Download the latest nightly version')
-
-        cls.parser.add_argument('--build', type=int, default=1, help='Specify build number (default 1)')
-        cls.parser.add_argument(
-            '--arch', choices=cls.arch_values, default='all',
-            help='Specify which architecture to get the apk for. Will download every architecture if not set.'
-        )
-        cls.parser.add_argument('--locale', default='multi', help='Specify which locale to get the apk for')
-        cls.parser.add_argument(
-            '--output-directory', dest='download_directory', default='apk-download',
-            help='Directory in which APKs will be downloaded to. Will be created if needed.'
-        )
+class GetAPK:
+    # TODO: version and latest_nightly are mutually exclusive, improve abstraction so this class isn't if/else-ing
+    def __init__(self, version, latest_nightly, build, arch, locale, download_directory):
+        self.version = version
+        self.latest_nightly = latest_nightly
+        self.build = build
+        self.arch = arch
+        self.locale = locale
+        self.download_directory = download_directory
 
     # Cleanup half downloaded files on Ctrl+C
     def signal_handler(self, signal, frame):
@@ -62,43 +47,22 @@ class GetAPK(Base):
 
     def cleanup(self):
         try:
-            shutil.rmtree(self.config.download_directory)
+            shutil.rmtree(self.download_directory)
             logger.info('Download directory cleaned')
         except FileNotFoundError:
-            logger.warning('{} was not found. Skipping...'.format(self.config.download_directory))
-
-    def generate_apk_base_url(self, version, build, locale, api_suffix):
-        return '{}/nightly/latest-mozilla-central-android-{}'.format(FTP_BASE_URL, api_suffix) \
-            if self.config.latest_nightly else \
-            '{}/android-{}/{}'.format(
-                generate_base_directory(version, build),
-                api_suffix,
-                locale,
-            )
-
-    def get_api_suffix(self, version, arch):
-        if arch != 'arm':
-            return [arch]
-        else:
-            api_levels = get_expected_api_levels(
-                version, get_firefox_package_name(version)
-            )
-            # TODO support old schemes when no API level was in the path
-            return [
-                'api-{}'.format(api_level) for api_level in api_levels
-            ]
+            logger.warning('{} was not found. Skipping...'.format(self.download_directory))
 
     async def download(self, session, version, build, architecture, locale):
         try:
-            os.makedirs(self.config.download_directory)
+            os.makedirs(self.download_directory)
         except FileExistsError:
             pass
 
-        for api_suffix in self.get_api_suffix(version, architecture):
-            apk_base_url = self.generate_apk_base_url(version, build, locale, api_suffix)
+        for api_suffix in get_api_suffix(version, architecture):
+            apk_base_url = generate_apk_base_url(self.latest_nightly, version, build, locale, api_suffix)
             urls_and_locations = craft_apk_and_checksums_url_and_download_locations(
-                apk_base_url, self.config.download_directory, version, build, locale, architecture,
-                api_suffix, self.config.latest_nightly
+                apk_base_url, self.download_directory, version, build, locale, architecture,
+                api_suffix, self.latest_nightly
             )
             apk = urls_and_locations['apk']
             checksums = urls_and_locations['checksums']
@@ -111,17 +75,17 @@ class GetAPK(Base):
             check_apk_against_checksum_file(apk['download_location'], checksums['download_location'])
 
     def get_version_name(self):
-        if self.config.latest_nightly:
-            json = load_json_url(self.json_version_url)
+        if self.latest_nightly:
+            json = load_json_url(JSON_VERSION_URL)
             version_code = json['FIREFOX_NIGHTLY']
             return version_code
-        return self.config.version
+        return self.version
 
     # Download all the archs if none is given
     async def download_all(self, session, version, build, locale):
         download_coroutines = [
             self.download(session, version, build, architecture, locale)
-            for architecture in self.arch_values
+            for architecture in ARCH_VALUES
         ]
         await asyncio.gather(*download_coroutines)
 
@@ -130,16 +94,16 @@ class GetAPK(Base):
         # Perhaps instead of custom validation, this behaviour/validation should happen in our argparse validation.
         # The only downside to specifying this within argparse is that the best solution (IMHO) is
         # with subcommands (https://stackoverflow.com/a/17909525), but that's a breaking change
-        if self.config.latest_nightly and (
-                self.config.build != self.parser.get_default('build') or
-                self.config.locale != self.parser.get_default('locale')):
+        if self.latest_nightly and (
+                self.build != self.parser.get_default('build') or
+                self.locale != self.parser.get_default('locale')):
             print('None of the arguments --build, --locale and --version can be used with --latest-nightly')
             sys.exit(1)
 
         version = self.get_version_name()
-        architecture = self.config.arch
-        build = str(self.config.build)
-        locale = self.config.locale
+        architecture = self.arch
+        build = str(self.build)
+        locale = self.locale
 
         logger.info('Downloading version "{}" build #{} for arch "{}" (locale "{}")'.format(version, build, architecture, locale))
 
@@ -148,6 +112,29 @@ class GetAPK(Base):
                 await self.download_all(session, version, build, locale)
             else:
                 await self.download(session, version, build, architecture, locale)
+
+
+def generate_apk_base_url(latest_nightly, version, build, locale, api_suffix):
+    return '{}/nightly/latest-mozilla-central-android-{}'.format(FTP_BASE_URL, api_suffix) \
+        if latest_nightly else \
+        '{}/android-{}/{}'.format(
+            generate_base_directory(version, build),
+            api_suffix,
+            locale,
+        )
+
+
+def get_api_suffix(version, arch):
+    if arch != 'arm':
+        return [arch]
+    else:
+        api_levels = get_expected_api_levels(
+            version, get_firefox_package_name(version)
+        )
+        # TODO support old schemes when no API level was in the path
+        return [
+            'api-{}'.format(api_level) for api_level in api_levels
+        ]
 
 
 def craft_apk_and_checksums_url_and_download_locations(base_apk_url, download_directory, version, build, locale,
@@ -248,7 +235,28 @@ if __name__ == '__main__':
     from mozapkpublisher.common import main_logging
     main_logging.init()
 
-    myScript = GetAPK()
+    parser = ArgumentParser(
+        description='Download APKs of Firefox for Android (aka Fennec) from {}'.format(FTP_BASE_URL)
+    )
+
+    exclusive_group = parser.add_mutually_exclusive_group(required=True)
+    exclusive_group.add_argument('--version', default=None, help='Specify version number to download (e.g. 23.0b7)')
+    exclusive_group.add_argument('--latest-nightly', action='store_true', default=False,
+                                 help='Download the latest nightly version')
+
+    parser.add_argument('--build', type=int, default=1, help='Specify build number (default 1)')
+    parser.add_argument(
+        '--arch', choices=ARCH_VALUES, default='all',
+        help='Specify which architecture to get the apk for. Will download every architecture if not set.'
+    )
+    parser.add_argument('--locale', default='multi', help='Specify which locale to get the apk for')
+    parser.add_argument(
+        '--output-directory', dest='download_directory', default='apk-download',
+        help='Directory in which APKs will be downloaded to. Will be created if needed.'
+    )
+
+    config = parser.parse_args()
+    myScript = GetAPK(config.version, config.latest_nightly, config.build, config.arch, config.locale, config.download_directory)
     signal.signal(signal.SIGINT, myScript.signal_handler)
     loop = asyncio.get_event_loop()
     loop.run_until_complete(myScript.run())

commit 1c357602b0b0f39c76083d497ecdb4fd6cec18a0

Remove get_apk.py
It's was broken 6 years ago by 1d4775dffde4a82ee3dca257f1f07d42c5a6f8c9
Since it's not used, is broken and references fennec artifacts which
haven't been a thing since firefox android migrated to m-c, just remove
it

mozapkpublisher/get_apk.py
@@ -1,262 +1,0 @@
-#!/usr/bin/env python3
-
-import aiohttp
-import asyncio
-import sys
-import os
-import re
-import signal
-import shutil
-import logging
-
-from argparse import ArgumentParser
-from mozapkpublisher.common.apk.history import (
-    get_expected_api_levels,
-    get_firefox_major_version_number,
-)
-from mozapkpublisher.common.exceptions import CheckSumMismatch
-from mozapkpublisher.common.utils import (
-    download_file,
-    file_sha512sum,
-    load_json_url,
-)
-
-logger = logging.getLogger(__name__)
-
-FTP_BASE_URL = 'https://ftp.mozilla.org/pub/mobile'
-ARCH_VALUES = ["arm", "x86"]
-JSON_VERSION_URL = "https://product-details.mozilla.org/1.0/firefox_versions.json"
-
-
-class GetAPK:
-    # TODO: version and latest_nightly are mutually exclusive, improve abstraction so this class isn't if/else-ing
-    def __init__(self, version, latest_nightly, build, arch, locale, download_directory):
-        self.version = version
-        self.latest_nightly = latest_nightly
-        self.build = build
-        self.arch = arch
-        self.locale = locale
-        self.download_directory = download_directory
-
-    # Cleanup half downloaded files on Ctrl+C
-    def signal_handler(self, signal, frame):
-        print("You pressed Ctrl+C!")
-        self.cleanup()
-        sys.exit(1)
-
-    def cleanup(self):
-        try:
-            shutil.rmtree(self.download_directory)
-            logger.info('Download directory cleaned')
-        except FileNotFoundError:
-            logger.warning('{} was not found. Skipping...'.format(self.download_directory))
-
-    async def download(self, session, version, build, architecture, locale):
-        try:
-            os.makedirs(self.download_directory)
-        except FileExistsError:
-            pass
-
-        for api_suffix in get_api_suffix(version, architecture):
-            apk_base_url = generate_apk_base_url(self.latest_nightly, version, build, locale, api_suffix)
-            urls_and_locations = craft_apk_and_checksums_url_and_download_locations(
-                apk_base_url, self.download_directory, version, build, locale, architecture,
-                api_suffix, self.latest_nightly
-            )
-            apk = urls_and_locations['apk']
-            checksums = urls_and_locations['checksums']
-
-            await asyncio.gather(
-                download_file(session, apk['url'], apk['download_location']),
-                download_file(session, checksums['url'], checksums['download_location'])
-            )
-
-            check_apk_against_checksum_file(apk['download_location'], checksums['download_location'])
-
-    def get_version_name(self):
-        if self.latest_nightly:
-            json = load_json_url(JSON_VERSION_URL)
-            version_code = json['FIREFOX_NIGHTLY']
-            return version_code
-        return self.version
-
-    # Download all the archs if none is given
-    async def download_all(self, session, version, build, locale):
-        download_coroutines = [
-            self.download(session, version, build, architecture, locale)
-            for architecture in ARCH_VALUES
-        ]
-        await asyncio.gather(*download_coroutines)
-
-    async def run(self):
-        # For latest-nightly, there's only one build, and the only locale is "en-US"
-        # Perhaps instead of custom validation, this behaviour/validation should happen in our argparse validation.
-        # The only downside to specifying this within argparse is that the best solution (IMHO) is
-        # with subcommands (https://stackoverflow.com/a/17909525), but that's a breaking change
-        if self.latest_nightly and (
-                self.build != self.parser.get_default('build') or
-                self.locale != self.parser.get_default('locale')):
-            print('None of the arguments --build, --locale and --version can be used with --latest-nightly')
-            sys.exit(1)
-
-        version = self.get_version_name()
-        architecture = self.arch
-        build = str(self.build)
-        locale = self.locale
-
-        logger.info('Downloading version "{}" build #{} for arch "{}" (locale "{}")'.format(version, build, architecture, locale))
-
-        async with aiohttp.ClientSession() as session:
-            if architecture == "all":
-                await self.download_all(session, version, build, locale)
-            else:
-                await self.download(session, version, build, architecture, locale)
-
-
-def generate_apk_base_url(latest_nightly, version, build, locale, api_suffix):
-    return '{}/nightly/latest-mozilla-central-android-{}'.format(FTP_BASE_URL, api_suffix) \
-        if latest_nightly else \
-        '{}/android-{}/{}'.format(
-            generate_base_directory(version, build),
-            api_suffix,
-            locale,
-        )
-
-
-def get_api_suffix(version, arch):
-    if arch != 'arm':
-        return [arch]
-    else:
-        api_levels = get_expected_api_levels(version)
-        # TODO support old schemes when no API level was in the path
-        return [
-            'api-{}'.format(api_level) for api_level in api_levels
-        ]
-
-
-def craft_apk_and_checksums_url_and_download_locations(base_apk_url, download_directory, version, build, locale,
-                                                       architecture, api_suffix, latest_nightly):
-    file_names = _craft_apk_and_checksums_file_names(version, locale, architecture)
-
-    urls_and_locations = {
-        extension: {
-            'download_location': os.path.join(download_directory, file_name),
-            'url': '/'.join([base_apk_url, file_name]),
-        } for extension, file_name in file_names.items()
-    }
-
-    if latest_nightly:
-        urls_and_locations['checksums']['url'] = \
-            '{}/nightly/latest-mozilla-central-android-{}/en-US/fennec-{}.en-US.android-{}.checksums'.format(
-                FTP_BASE_URL, api_suffix, version, _get_architecture_in_file_name(architecture)
-            )
-    elif get_firefox_major_version_number(version) >= 59:
-        urls_and_locations['checksums']['url'] = '{}/{}'.format(
-            generate_base_directory(version, build), 'SHA512SUMS'
-        )
-
-    return urls_and_locations
-
-
-def generate_base_directory(version, build):
-    return '{}/candidates/{}-candidates/build{}'.format(FTP_BASE_URL, version, build)
-
-
-def _craft_apk_and_checksums_file_names(version, locale, architecture):
-    file_name_architecture = _get_architecture_in_file_name(architecture)
-    extensions = ['apk', 'checksums']
-
-    return {
-        extension: 'fennec-{}.{}.android-{}.{}'.format(version, locale, file_name_architecture, extension)
-        for extension in extensions
-    }
-
-
-def _get_architecture_in_file_name(architecture):
-    # the file name contains i386 instead of x86
-    return 'i386' if architecture == 'x86' else architecture
-
-
-def check_apk_against_checksum_file(apk_file, checksum_file):
-    logger.debug('Checking checksum for "{}"...'.format(apk_file))
-
-    checksum = _fetch_checksum_from_file(checksum_file, apk_file)
-    apk_checksum = file_sha512sum(apk_file)
-
-    if checksum == apk_checksum:
-        logger.info('Checksum for "{}" succeeded!'.format(apk_file))
-        os.remove(checksum_file)
-    else:
-        raise CheckSumMismatch(apk_file, expected=apk_checksum, actual=checksum)
-
-
-def _fetch_checksum_from_file(checksum_file, apk_file):
-    base_apk_filepath = _take_out_common_path(checksum_file, apk_file)
-
-    # pre-Fennec 58 style
-    checksum = _match_checksum_regex(
-        checksum_file,  r"""^(?P<hash>.*) sha512 (?P<filesize>\d+) {}""".format(base_apk_filepath)
-    )
-
-    if not checksum:
-        # post-Fennec 58. More greedy
-        checksum = _match_checksum_regex(
-            checksum_file,  r"""^(?P<hash>.*)  {}""".format(base_apk_filepath)
-        )
-
-        if not checksum:
-            # old style pre-Fennec 53 checksums files. Super greedy
-            with open(checksum_file, 'r') as f:
-                checksum = f.read()
-            checksum = re.sub(r'\s(.*)', '', checksum.splitlines()[0])
-
-    logger.info("Found hash {}".format(checksum))
-    return checksum
-
-
-def _match_checksum_regex(checksum_file, regex):
-    with open(checksum_file, 'r') as fh:
-        for line in fh:
-            m = re.match(regex, line)
-            if m:
-                gd = m.groupdict()
-                return gd['hash']
-    return None
-
-
-def _take_out_common_path(checksum_file, apk_file):
-    return os.path.relpath(apk_file, os.path.dirname(checksum_file))
-
-
-def main():
-    from mozapkpublisher.common import main_logging
-    main_logging.init()
-
-    parser = ArgumentParser(
-        description='Download APKs of Firefox for Android (aka Fennec) from {}'.format(FTP_BASE_URL)
-    )
-
-    exclusive_group = parser.add_mutually_exclusive_group(required=True)
-    exclusive_group.add_argument('--version', default=None, help='Specify version number to download (e.g. 23.0b7)')
-    exclusive_group.add_argument('--latest-nightly', action='store_true', default=False,
-                                 help='Download the latest nightly version')
-
-    parser.add_argument('--build', type=int, default=1, help='Specify build number (default 1)')
-    parser.add_argument(
-        '--arch', choices=ARCH_VALUES, default='all',
-        help='Specify which architecture to get the apk for. Will download every architecture if not set.'
-    )
-    parser.add_argument('--locale', default='multi', help='Specify which locale to get the apk for')
-    parser.add_argument(
-        '--output-directory', dest='download_directory', default='apk-download',
-        help='Directory in which APKs will be downloaded to. Will be created if needed.'
-    )
-
-    config = parser.parse_args()
-    myScript = GetAPK(config.version, config.latest_nightly, config.build, config.arch, config.locale, config.download_directory)
-    signal.signal(signal.SIGINT, myScript.signal_handler)
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(myScript.run())
-
-
-__name__ == '__main__' and main()

mozapkpublisher/test/integration/test_get_apk.py
@@ -1,33 +1,0 @@
-import pytest
-import os
-import re
-import shutil
-import tempfile
-
-from distutils.util import strtobool
-
-from mozapkpublisher.get_apk import GetAPK
-
-
-@pytest.mark.asyncio
-@pytest.mark.skipif(strtobool(os.environ.get('SKIP_NETWORK_TESTS', 'true')), reason='Tests requiring network are skipped')
-@pytest.mark.parametrize('version, build, arch, apks_file_regexes', (
-    # Re-enable latest_nightly once bug 1346752 is fixed
-    # ({'latest_nightly': True, 'arch': 'x86'}, (r'fennec-\d{2}\.0a1\.multi\.android-i386\.apk',)),
-
-    # Pre-Fennec 53.0b1
-    ('52.0', '2', 'arm', (r'fennec-52\.0\.multi\.android-arm\.apk',)),
-    ('53.0b1', '3', 'arm', (r'fennec-53\.0b1\.multi\.android-arm\.apk',)),
-))
-async def test_download_files(version, build, arch, apks_file_regexes):
-    temp_dir = tempfile.mkdtemp()
-    await GetAPK(version, None, build, arch, 'multi', temp_dir).run()
-    files_in_temp_dir = [
-        name for name in os.listdir(temp_dir) if os.path.isfile(os.path.join(temp_dir, name))
-    ]
-    assert len(files_in_temp_dir) == len(apks_file_regexes)
-
-    for regex in apks_file_regexes:
-        assert any(re.match(regex, file_name) for file_name in files_in_temp_dir)
-
-    shutil.rmtree(temp_dir)

mozapkpublisher/test/test_get_apk.py
@@ -1,160 +1,0 @@
-import mock
-import os
-import pytest
-import shutil
-import sys
-from tempfile import mkdtemp
-
-from mozapkpublisher.common.exceptions import CheckSumMismatch
-from mozapkpublisher.get_apk import main, craft_apk_and_checksums_url_and_download_locations, \
-    _craft_apk_and_checksums_file_names, _get_architecture_in_file_name, check_apk_against_checksum_file, \
-    _fetch_checksum_from_file, _take_out_common_path, get_api_suffix, generate_apk_base_url
-
-DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
-CHECKSUM_APK = os.path.join(DATA_DIR, 'blob')
-
-
-def test_generate_apk_base_url():
-    assert generate_apk_base_url(True, '52.0a1', None, 'multi', 'x86') == \
-        'https://ftp.mozilla.org/pub/mobile/nightly/latest-mozilla-central-android-x86'
-
-    assert generate_apk_base_url(False, '50.0b8', 1, 'multi', 'api-15') == \
-        'https://ftp.mozilla.org/pub/mobile/candidates/50.0b8-candidates/build1/android-api-15/multi'
-
-
-@pytest.mark.parametrize('version, architecture, expected', ((
-    '45.0', 'arm', ['api-9', 'api-11'],
-), (
-    '47.0', 'arm', ['api-9', 'api-15'],
-), (
-    '55.0', 'arm', ['api-15'],
-), (
-    '55.0', 'x86', ['x86'],
-), (
-    '56.0', 'arm', ['api-16'],
-)))
-def test_get_api_suffix(version, architecture, expected):
-    assert get_api_suffix(version, architecture) == expected
-
-
-def test_craft_apk_and_checksums_url_and_download_locations():
-    assert craft_apk_and_checksums_url_and_download_locations(
-        'https://ftp.mozilla.org/pub/mobile/candidates/64.0b3-candidates/build1/android-api-16/multi',
-        '/a/fake/download/directory', '64.0b3', 1, 'multi', 'arm', 'api-16', False
-    ) == {
-        'apk': {
-            'url': 'https://ftp.mozilla.org/pub/mobile/candidates/64.0b3-candidates/build1/android-api-16/multi/fennec-64.0b3.multi.android-arm.apk',
-            'download_location': '/a/fake/download/directory/fennec-64.0b3.multi.android-arm.apk',
-        },
-        'checksums': {
-            'url': 'https://ftp.mozilla.org/pub/mobile/candidates/64.0b3-candidates/build1/SHA512SUMS',
-            'download_location': '/a/fake/download/directory/fennec-64.0b3.multi.android-arm.checksums',
-        },
-    }
-    assert craft_apk_and_checksums_url_and_download_locations(
-        'https://ftp.mozilla.org/pub/mobile/candidates/52.0b1-candidates/build1/android-api-15/multi',
-        '/a/fake/download/directory', '52.0b1', 1, 'multi', 'arm', 'api-15', False
-    ) == {
-           'apk': {
-               'url': 'https://ftp.mozilla.org/pub/mobile/candidates/52.0b1-candidates/build1/android-api-15/multi/fennec-52.0b1.multi.android-arm.apk',
-               'download_location': '/a/fake/download/directory/fennec-52.0b1.multi.android-arm.apk',
-           },
-           'checksums': {
-               'url': 'https://ftp.mozilla.org/pub/mobile/candidates/52.0b1-candidates/build1/android-api-15/multi/fennec-52.0b1.multi.android-arm.checksums',
-               'download_location': '/a/fake/download/directory/fennec-52.0b1.multi.android-arm.checksums',
-           },
-       }
-    assert craft_apk_and_checksums_url_and_download_locations(
-        'https://ftp.mozilla.org/pub/mobile/nightly/latest-mozilla-central-android-api-15',
-        '/a/fake/download/directory', '53.0a1', 1, 'multi', 'arm', 'api-15', True
-    ) == {
-        'apk': {
-            'url': 'https://ftp.mozilla.org/pub/mobile/nightly/latest-mozilla-central-android-api-15/fennec-53.0a1.multi.android-arm.apk',
-            'download_location': '/a/fake/download/directory/fennec-53.0a1.multi.android-arm.apk',
-        },
-        'checksums': {
-            'url': 'https://ftp.mozilla.org/pub/mobile/nightly/latest-mozilla-central-android-api-15/en-US/fennec-53.0a1.en-US.android-arm.checksums',
-            'download_location': '/a/fake/download/directory/fennec-53.0a1.multi.android-arm.checksums',
-        },
-    }
-
-
-def test_craft_apk_and_checksums_file_names():
-    assert _craft_apk_and_checksums_file_names('50.0b8', 'multi', 'arm') == {
-        'apk': 'fennec-50.0b8.multi.android-arm.apk',
-        'checksums': 'fennec-50.0b8.multi.android-arm.checksums',
-    }
-    assert _craft_apk_and_checksums_file_names('51.0a2', 'en-US', 'x86') == {
-        'apk': 'fennec-51.0a2.en-US.android-i386.apk',
-        'checksums': 'fennec-51.0a2.en-US.android-i386.checksums',
-    }
-
-
-def test_get_architecture_in_file_name():
-    assert _get_architecture_in_file_name('arm') == 'arm'
-    assert _get_architecture_in_file_name('x86') == 'i386'
-
-
-@pytest.mark.parametrize('checksum_file,raises', ((
-    os.path.join(DATA_DIR, 'checksums.old'), False,
-), (
-    os.path.join(DATA_DIR, 'checksums.tc'), False,
-), (
-    os.path.join(DATA_DIR, 'checksums.broken'), True,
-)))
-def test_check_apk_against_checksum_file(checksum_file, raises):
-    try:
-        # check_apk nukes the checksum file, so make a copy first
-        temp_dir = mkdtemp()
-        temp_checksum_file = os.path.join(temp_dir, os.path.basename(checksum_file))
-        shutil.copyfile(checksum_file, temp_checksum_file)
-
-        # paths in checksum are relatives, so make a copy of the blob as well
-        temp_apk_file = os.path.join(temp_dir, os.path.basename(CHECKSUM_APK))
-        shutil.copyfile(CHECKSUM_APK, temp_apk_file)
-        with mock.patch.object(shutil, 'rmtree'):
-            if raises:
-                with pytest.raises(CheckSumMismatch):
-                    check_apk_against_checksum_file(temp_apk_file, temp_checksum_file)
-            else:
-                check_apk_against_checksum_file(temp_apk_file, temp_checksum_file)
-    finally:
-        shutil.rmtree(temp_dir)
-
-
-@pytest.mark.parametrize('checksum_file', (
-    os.path.join(DATA_DIR, 'checksums.old'),
-    os.path.join(DATA_DIR, 'checksums.tc'),
-    os.path.join(DATA_DIR, 'SHA512SUMS'),
-))
-def test_fetch_checksum_from_file(checksum_file):
-    assert _fetch_checksum_from_file(checksum_file, CHECKSUM_APK) == \
-        'd5ee2608eb21d827deef87732dc4796c7209098b8db39f95c3fac87c0dc7b186f2b097f0a52e856e6ac504ff3039a3e23936615be55a172665cdd0250f3a4379'
-
-
-@pytest.mark.parametrize('checksum_file, apk_file, expected', (
-    (
-        '/a/fake/download/directory/target.checksums',
-        '/a/fake/download/directory/target.apk',
-        'target.apk'
-    ),
-    (
-        '/a/fake/download/directory/fennec-50.0b8.multi.android-arm.checksums',
-        '/a/fake/download/directory/fennec-50.0b8.multi.android-arm.apk',
-        'fennec-50.0b8.multi.android-arm.apk'
-    ),
-    (
-        '/a/fake/download/directory/target.checksums',
-        '/a/fake/download/directory/a-subdir/target.apk',
-        'a-subdir/target.apk'
-    ),
-))
-def test_take_out_common_path(checksum_file, apk_file, expected):
-    assert _take_out_common_path(checksum_file, apk_file) == expected
-
-
-def test_exits_if_invalid_arguments(monkeypatch):
-    incomplete_args = ['script']
-    monkeypatch.setattr(sys, 'argv', incomplete_args)
-    with pytest.raises(SystemExit):
-        main()